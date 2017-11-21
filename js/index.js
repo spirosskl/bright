96172d7cf432ba5f899806c510a78f656aa5b5c5
spirosskl <33687093+spirosskl@users.noreply.github.com>
2017-11-21 13:05:11 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,46 +1,27 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
- document.addEventListener("deviceready", onDeviceReady, false); 
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+// device APIs are available
+//
 function onDeviceReady() {
-    // Now safe to use the Cordova API
-    alert('sdf');
- window.location="http://your.website";
+     window.location="http://bright.com";
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+    // Add similar listeners for other events
 }
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+function onPause() {
+    // Handle the pause event
+}
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        //this.receivedEvent('deviceready');
-        window.location="http://www.bright.gr";
-    },
+function onResume() {
+    // Handle the resume event
+}
 
-    // Update DOM on a Received Event
-    
-};
+function onMenuKeyDown() {
+    // Handle the menubutton event
+}
 
-app.initialize();
+// Add similar event handlers for other events
