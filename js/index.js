ef2548417eaa6e82247579de0342b549db31abe6
spirosskl <33687093+spirosskl@users.noreply.github.com>
2017-11-21 12:57:12 +0200

    update

--- js/index.js
+++ js/index.js
@@ -20,5 +20,35 @@
  document.addEventListener("deviceready", onDeviceReady, false); 
 function onDeviceReady() {
     // Now safe to use the Cordova API
- window.location="http://bright.gr";
+ window.location="http://your.website";
 }
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+        window.location="http://www.bright.gr";
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
