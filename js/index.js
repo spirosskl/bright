f6b47787a145574bcbe468aa2b01a922d55e8608
spirosskl <33687093+spirosskl@users.noreply.github.com>
2017-11-23 11:02:59 +0200

    update

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        alert('df');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
